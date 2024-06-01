f32695022e59ce7fdc1d8a64281bb6a389474f80
kashkostwebsites <155662960+kashkostwebsites@users.noreply.github.com>
2024-06-01 12:48:07 +0100

    save2

--- src/App.js
+++ src/App.js
@@ -2,32 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 import React, { useState } from 'react';
 
-
-
-
-function varfix() {
-const [count, setCount] = useState(0);
-}
-
-function handleclick() {
- varfix.setCount(count + 1);
-}
-
 function App() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-       <h1>How much can you click!</h1>
-        <a
-          onClick="handleclick()"
-        >
+        <h1>How much can you click!</h1>
+        <a onClick={handleClick}>
           Click Now! 
         </a>
-
-        <p>Clicked: {varfix.count} times</p>
+        <p>Clicked: {count} times</p>
       </header>
     </div>
   );
 }
 
 export default App;
+
