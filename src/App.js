77072d0d11c82ed5ba6e3e9a0bf8864e33413616
kashkostwebsites <155662960+kashkostwebsites@users.noreply.github.com>
2024-06-01 12:23:21 +0100

    add

--- src/App.js
+++ src/App.js
@@ -10,10 +10,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+       <h1>How much can you click!</h1>
         <a
           onClick="handleclick()"
         >
